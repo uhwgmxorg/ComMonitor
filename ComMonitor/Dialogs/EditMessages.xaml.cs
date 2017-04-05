5404b1e3affc7f00afada2f525fe4c38dd53e94a
Uwe <uhw@gmx.org>
2017-04-05 23:01:14 +0200

    Try to implement edible TabItem-Header, but not working yet.

--- ComMonitor/Dialogs/EditMessages.xaml.cs
+++ ComMonitor/Dialogs/EditMessages.xaml.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
 using WPFHexaEditor.Control;
 
 namespace ComMonitor.Dialogs
@@ -95,8 +97,8 @@ namespace ComMonitor.Dialogs
             {
                 foreach (var t in TabItems)
                 {
-                    t.HexEditor.SubmitChanges();
-                    MessagesToEdit[i++] = t.HexEditor.Stream.ToArray();
+                    //t.HexEditor.SubmitChanges();
+                    //MessagesToEdit[i++] = t.HexEditor.Stream.ToArray();
                 }
                 FocusMessage = MessagesToEdit[tabHexaEditors.SelectedIndex];
             }
@@ -134,17 +136,10 @@ namespace ComMonitor.Dialogs
         {
             int count = TabItems.Count;
 
-            // create new tab item
             TabItem tab = new TabItem();
-
             tab.Header = String.Format("Message {0}", count + 1);
             tab.HexEditor = new HexaEditor();
-            tab.HexEditor.Width = Double.NaN;
-            tab.HexEditor.Height = Double.NaN;
-            tab.HexEditor.Stream = new System.IO.MemoryStream(v);
-
             TabItems.Add(tab);
-
             _logger.Trace(String.Format("AddTabItem in {0}", LST.GetCurrentMethod()));
 
             return tab;
@@ -158,4 +153,115 @@ namespace ComMonitor.Dialogs
         public string Header { get; set; }
         public HexaEditor HexEditor { get; set; }
     }
+
+    [TemplatePart(Name = "PART_TabHeader", Type = typeof(TextBox))]
+    public class EditableTabHeaderControl : ContentControl
+    {
+        /// <summary>
+        /// Dependency property to bind EditMode with XAML Trigger
+        /// </summary>
+        private static readonly DependencyProperty IsInEditModeProperty = DependencyProperty.Register("IsInEditMode", typeof(bool), typeof(EditableTabHeaderControl));
+        private TextBox textBox;
+        private string oldText;
+        private DispatcherTimer timer;
+        private delegate void FocusTextBox();
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is in edit mode.
+        /// </summary>
+        public bool IsInEditMode
+        {
+            get
+            {
+                return (bool)this.GetValue(IsInEditModeProperty);
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(this.textBox.Text))
+                {
+                    this.textBox.Text = this.oldText;
+                }
+
+                this.oldText = this.textBox.Text;
+                this.SetValue(IsInEditModeProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, is invoked whenever application code or internal processes call <see cref="M:System.Windows.FrameworkElement.ApplyTemplate"/>.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this.textBox = this.Template.FindName("PART_TabHeader", this) as TextBox;
+            if (this.textBox != null)
+            {
+                this.timer = new DispatcherTimer();
+                this.timer.Tick += TimerTick;
+                this.timer.Interval = TimeSpan.FromMilliseconds(1);
+                this.LostFocus += TextBoxLostFocus;
+                this.textBox.KeyDown += TextBoxKeyDown;
+                this.MouseDoubleClick += EditableTabHeaderControlMouseDoubleClick;
+            }
+        }
+
+        /// <summary>
+        /// Sets the IsInEdit mode.
+        /// </summary>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        public void SetEditMode(bool value)
+        {
+            this.IsInEditMode = value;
+            this.timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            this.timer.Stop();
+            this.MoveTextBoxInFocus();
+        }
+
+        private void MoveTextBoxInFocus()
+        {
+            if (this.textBox.CheckAccess())
+            {
+                if (!string.IsNullOrEmpty(this.textBox.Text))
+                {
+                    this.textBox.CaretIndex = 0;
+                    this.textBox.Focus();
+                }
+            }
+            else
+            {
+                this.textBox.Dispatcher.BeginInvoke(DispatcherPriority.Render, new FocusTextBox(this.MoveTextBoxInFocus));
+            }
+        }
+
+        private void TextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.textBox.Text = oldText;
+                this.IsInEditMode = false;
+            }
+            else if (e.Key == Key.Enter)
+            {
+                this.IsInEditMode = false;
+            }
+        }
+
+        private void TextBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            this.IsInEditMode = false;
+        }
+
+        private void EditableTabHeaderControlMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.SetEditMode(true);
+            }
+        }
+    }
+                 
 }
