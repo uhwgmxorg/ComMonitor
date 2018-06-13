9575518ff8d0174fda6a8daddcb7a4718beacd77
Uwe Walter <uhw@gmx.org>
2018-06-13 17:40:28 +0200

    Add NewItem.

--- PingLib/PingToolUC.xaml.cs
+++ PingLib/PingToolUC.xaml.cs
@@ -10,6 +10,7 @@ using System.Diagnostics;
 using LiveCharts;
 using LiveCharts.Wpf;
 using LiveCharts.Configurations;
+using System.Collections.ObjectModel;
 
 namespace PingLib
 {
@@ -44,6 +45,35 @@ namespace PingLib
             get { return series; }
             set { SetField(ref this.series, value, nameof(Series)); }
         }
+
+        private string selectedIp;
+        public string SelectedIp
+        {
+            get { return selectedIp; }
+            set { SetField(ref this.selectedIp, value, nameof(SelectedIp)); }
+        }
+        private ObservableCollection<string> ipList;
+        public ObservableCollection<string> IpList
+        {
+            get { return ipList; }
+            set { SetField(ref this.ipList, value, nameof(IpList)); }
+        }
+        public string NewItem
+        {
+            set
+            {
+                if (SelectedIp != null)
+                {
+                    return;
+                }
+                if (!string.IsNullOrEmpty(value))
+                {
+                    ipList.Add(value);
+                    SelectedIp = value;
+                }
+            }
+        }
+
         #endregion
 
         public Func<double, string> Formatter { get; set; }
@@ -56,12 +86,6 @@ namespace PingLib
             InitializeComponent();
             DataContext = this;
 
-            // Init timer
-            _dispatcherTimer = new DispatcherTimer();
-            _dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
-            _dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
-            _dispatcherTimer.Start();
-
             // Init chart
             var config = Mappers.Xy<ChartDataModel>()
                 .X(dayModel => (double)dayModel.DateTime.Ticks / TimeSpan.FromHours(1).Ticks)
@@ -70,12 +94,54 @@ namespace PingLib
             Series[0].Values.Clear();
             ((Series)Series[0]).Fill = new SolidColorBrush(Color.FromArgb(255, (byte)153, (byte)180, (byte)211));
             Formatter = value => DateTime.Now.ToString("d/M/yyyy HH:mm:ss");
+
+            Button_Stop.IsEnabled = false;
+            Button_Start.IsEnabled = true;
+
+            IpList = new ObservableCollection<string>();
+            IpList.Add("127.0.0.1");
+            SelectedIp = IpList[0];
         }
 
         /******************************/
         /*       Button Events        */
         /******************************/
         #region Button Events
+
+        /// <summary>
+        /// Button_Start_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Start_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (_dispatcherTimer != null)
+                return;
+            // Init timer
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+            _dispatcherTimer.Start();
+            Button_Stop.IsEnabled = true;
+            Button_Start.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// Button_Stop_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Stop_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (_dispatcherTimer == null)
+                return;
+            // Init timer
+            _dispatcherTimer.Stop();
+            _dispatcherTimer.Tick -= new EventHandler(DispatcherTimer_Tick);
+            _dispatcherTimer = null;
+            Button_Stop.IsEnabled = false;
+            Button_Start.IsEnabled = true;
+        }
 
         #endregion
         /******************************/
@@ -96,7 +162,7 @@ namespace PingLib
         /// <param name="e"></param>
         private void DispatcherTimer_Tick(object sender, EventArgs e)
         {
-            Series[0].Values.Add(new ChartDataModel { DateTime = System.DateTime.Now.AddHours(++_counter), Value = LocalPing.Ping("superuser.com") });
+            Series[0].Values.Add(new ChartDataModel { DateTime = System.DateTime.Now.AddHours(++_counter), Value = LocalPing.Ping(SelectedIp) });
             Debug.WriteLine(String.Format("{0} {1}",((ChartDataModel)Series[0].Values[Series[0].Values.Count-1]).DateTime, ((ChartDataModel)Series[0].Values[Series[0].Values.Count - 1]).Value));
             if (Series[0].Values.Count > 30)
                 Series[0].Values.RemoveAt(0);
@@ -109,6 +175,7 @@ namespace PingLib
         /// <param name="e"></param>
         private void UserControl_Unloaded(object sender, System.Windows.RoutedEventArgs e)
         {
+            if (_dispatcherTimer == null) return;
             _dispatcherTimer.Tick -= new EventHandler(DispatcherTimer_Tick);
             _dispatcherTimer.Stop();
         }
